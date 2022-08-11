fdfd731885ecc82cff33737138a7e2f9f9fa4963
Jack <2577806591@qq.com>
2022-08-11 22:04:45 +0800

    first commit

---
+++ src/main/java/com/wit/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wit.git;/**
+ * @author JackYang
+ * @create 2022-08-11 22:01
+ */
+
+/**
+ * @description:
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
