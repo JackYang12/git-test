1b52fa12eb7c31a6de130255b1fa61a0df795a76
Jack <2577806591@qq.com>
2022-08-12 16:56:29 +0800

    second commit

--- src/main/java/com/wit/git/GitTest.java
+++ src/main/java/com/wit/git/GitTest.java
@@ -9,5 +9,6 @@ package com.wit.git;/**
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World git");
     }
 }
