2bcf09dcb3b27fbea973035e0a6d5bbaf77e92ed
Jack <2577806591@qq.com>
2022-08-12 16:57:39 +0800

    third commit

--- src/main/java/com/wit/git/GitTest.java
+++ src/main/java/com/wit/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World git");
+        System.out.println("Hello World git3");
     }
 }
